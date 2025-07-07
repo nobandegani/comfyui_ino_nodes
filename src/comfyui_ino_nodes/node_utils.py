c5575f966aa1829e222defb3a2f0f54b177ddc66
Inoland <contact@nobandegan.com>
2025-07-07 12:54:37 +0400

    add caclucalte lora config

--- src/comfyui_ino_nodes/node_utils.py
+++ src/comfyui_ino_nodes/node_utils.py
@@ -720,3 +720,68 @@ class InoRandomCharacterPrompt:
             m = hashlib.sha256()
             m.update(seed)
             return m.digest().hex()
+
+
+
+
+class InoCalculateLoraConfig:
+    """
+
+    """
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "enabled": ("BOOLEAN", {"default": True, "label_off": "OFF", "label_on": "ON"}),
+                "dataset_count": ("INT", {
+                    "default": 6,
+                    "step": 1,
+                    "display": "number"
+                }),
+            },
+            "optional": {
+                "max_batch_size": ("INT", {
+                    "default": 6,
+                    "step": 1,
+                    "display": "number"
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("INT", "INT", "INT", "INT", "INT",)
+    RETURN_NAMES = ("steps", "batch_size", "dim", "alpha", "learning rate")
+    DESCRIPTION = cleandoc(__doc__)
+    FUNCTION = "function"
+
+    CATEGORY = "InoNodes"
+
+    def __init__(self):
+        pass
+
+
+    def function(self, enabled, dataset_count, max_batch_size):
+        if not enabled:
+            return 0, 0, 0, 0, 0
+
+        steps = int(dataset_count * 60)
+
+        batch_size = min(max_batch_size, max(1, dataset_count // 5))
+
+        if dataset_count < 40:
+            dim = 8
+        elif dataset_count < 80:
+            dim = 16
+        else:
+            dim = 32
+
+        alpha = dim // 2
+
+        if dataset_count <= 40:
+            lr = 0.00005
+        elif dataset_count <= 100:
+            lr = 0.0001
+        else:
+            lr = 0.00015
+
+        return steps, batch_size, dim, alpha, lr
