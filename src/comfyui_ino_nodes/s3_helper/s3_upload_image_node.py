c229f06d70feb8dd8ccea23575c4dfa37a13c35b
Inoland <contact@nobandegan.com>
2025-09-30 16:14:23 +0400

    Update s3_upload_image_node.py

--- src/comfyui_ino_nodes/s3_helper/s3_upload_image_node.py
+++ src/comfyui_ino_nodes/s3_helper/s3_upload_image_node.py
@@ -1,6 +1,7 @@
 import os
 import json
 import tempfile
+from pathlib import Path
 import numpy as np
 from PIL import Image
 from PIL.PngImagePlugin import PngInfo
@@ -10,80 +11,54 @@ from .s3_client import get_s3_instance
 S3_INSTANCE = get_s3_instance()
 
 class InoS3UploadImage:
-    def __init__(self):
-        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-        self.temp_dir = os.path.join(base_dir, "temp/")
-        self.s3_output_dir = os.getenv("S3_OUTPUT_DIR")
-        self.type = "output"
-        self.prefix_append = ""
-        self.compress_level = 4
-
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {
-            "images": ("IMAGE",),
-            "filename_prefix": ("STRING", {"default": "Image"})},
-            "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"
-                       },
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "s3_key": ("STRING", {"default": ""}),
+                "file_name": ("STRING", {"default": ""})
+            }
         }
 
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("s3_image_paths",)
+    RETURN_TYPES = ("BOOLEAN", "STRING", "STRING", "STRING",)
+    RETURN_NAMES = ("success", "msg", "result", "s3_image_paths",)
     FUNCTION = "function"
-    OUTPUT_NODE = True
-    OUTPUT_IS_LIST = (True,)
     CATEGORY = "InoS3Helper"
 
-    def function(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
-        filename_prefix += self.prefix_append
-        full_output_folder, filename, counter, subfolder, filename_prefix = S3_INSTANCE.get_save_path(filename_prefix,
-                                                                                                      images[0].shape[
-                                                                                                          1],
-                                                                                                      images[0].shape[
-                                                                                                          0])
+    async def function(self, images, s3_key, file_name):
         results = list()
         s3_image_paths = list()
 
+        counter = 0
         for image in images:
             i = 255. * image.cpu().numpy()
             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+
             metadata = None
             if not args.disable_metadata:
                 metadata = PngInfo()
-                if prompt is not None:
-                    metadata.add_text("prompt", json.dumps(prompt))
-                if extra_pnginfo is not None:
-                    for x in extra_pnginfo:
-                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
 
-            file = f"{filename}_{counter:05}_.png"
-            temp_file = None
+            file = f"{file_name}_{counter:05}.png"
+            temp_file: Path = Path("input/temp_file.png")
             try:
-                # Create a temporary file
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
-                    temp_file_path = temp_file.name
+                img.save(temp_file, pnginfo=metadata)
 
-                    # Save the image to the temporary file
-                    img.save(temp_file_path, pnginfo=metadata, compress_level=self.compress_level)
+                s3_path:Path = Path(s3_key) / Path(file)
 
-                    # Upload the temporary file to S3
-                    s3_path = os.path.join(full_output_folder, file)
-                    file_path = S3_INSTANCE.upload_file(temp_file_path, s3_path)
+                uploaded = await S3_INSTANCE.upload_file(
+                    s3_key=str(s3_path),
+                    local_file_path=temp_file
+                )
+                if not uploaded["success"]:
+                    return (uploaded["success"], uploaded["msg"], results, s3_image_paths,  )
 
-                    # Add the s3 path to the s3_image_paths list
-                    s3_image_paths.append(file_path)
+                s3_image_paths.append(s3_path)
 
-                    # Add the result to the results list
-                    results.append({
-                        "filename": file,
-                        "subfolder": subfolder,
-                        "type": self.type
-                    })
-                    counter += 1
-
+                results.append(uploaded)
+                counter += 1
             finally:
-                # Delete the temporary file
-                if temp_file_path and os.path.exists(temp_file_path):
-                    os.remove(temp_file_path)
+                if temp_file.exists():
+                    os.remove(temp_file)
 
-        return {"ui": {"images": results}, "result": (s3_image_paths,)}
+        return (True, "sucess", results, s3_image_paths, )
