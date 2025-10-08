7320e95625c6405eab0a748ae96f82412241a675
Inoland <contact@nobandegan.com>
2025-10-08 16:23:55 +0400

    Update http_helper.py

--- src/comfyui_ino_nodes/utils/http_helper.py
+++ src/comfyui_ino_nodes/utils/http_helper.py
@@ -1,5 +1,8 @@
 import httpx
+import json
 from httpx_retry import AsyncRetryTransport, RetryPolicy
+
+from inopyutils import InoJsonHelper
 
 class InoHttpCall:
     """
@@ -11,10 +14,16 @@ class InoHttpCall:
         return {
             "required": {
                 "enabled": ("BOOLEAN", {"default": True, "label_off": "OFF", "label_on": "ON"}),
-                "url": ("STRING", {"default": "http://127.0.0.1:3314/health"}),
+                "url": ("STRING", {"default": "http://127.0.0.1:3313/health"}),
                 "request_type": (list(["get", "post", "put", "delete", "patch",]), {}),
                 "headers": ("STRING", {"default": '{"Connection": "keep-alive"}'}),
-                "json": ("STRING", {"default": None})
+                "json_payload": ("STRING", {"default": None})
+            },
+            "optional": {
+                "trust_env": ("BOOLEAN", {"default": False}),
+                "follow_redirects": ("BOOLEAN", {"default": False}),
+                "enable_retries": ("BOOLEAN", {"default": True}),
+                "max_retries": ("INT", {"default": 10, "min": 1, "max": 50}),
             }
         }
 
@@ -24,42 +33,57 @@ class InoHttpCall:
     FUNCTION = "function"
     CATEGORY = "InoNodes"
 
-    async def function(self, enabled, url, request_type, headers, json):
+    async def function(self, enabled,
+                       url, request_type, headers, json_payload,
+                       trust_env, follow_redirects, enable_retries, max_retries
+                       ):
         if not enabled:
             return (False, 0, "", "", )
 
         timeout = httpx.Timeout(connect=10.0, read=60, write=60, pool=10.0)
         limits = httpx.Limits(max_connections=8, max_keepalive_connections=8, keepalive_expiry=120.0)
 
-        exponential_retry = (
-            RetryPolicy()
-            .with_max_retries(10)
-            .with_min_delay(0.1)
-            .with_multiplier(2)
-            .with_retry_on(lambda status_code: status_code >= 500)
-        )
+        if enable_retries:
+            exponential_retry = (
+                RetryPolicy()
+                .with_max_retries(max_retries)
+                .with_min_delay(0.1)
+                .with_multiplier(2)
+                .with_retry_on(lambda status_code: status_code >= 500)
+            )
+        else:
+            exponential_retry = None
 
-        headers = headers
+        if InoJsonHelper.is_valid(headers):
+            headers = InoJsonHelper.string_to_dict(headers)
+        else:
+            headers = {}
+
+        if InoJsonHelper.is_valid(json_payload):
+            json_payload = InoJsonHelper.string_to_dict(json_payload)
+        else:
+            json_payload = {}
+
         http_conn = httpx.AsyncClient(
             timeout=timeout,
             limits=limits,
-            #transport=AsyncRetryTransport(policy=exponential_retry),
+            transport=AsyncRetryTransport(policy=exponential_retry),
             headers=headers,
-            #trust_env=False,
-            #follow_redirects=False
+            trust_env=trust_env,
+            follow_redirects=follow_redirects
         )
 
         try:
             if request_type == "get":
                 resp = await http_conn.get(url)
             elif request_type == "post":
-                resp = await http_conn.post(url, json=json)
+                resp = await http_conn.post(url, json=json_payload)
             elif request_type == "put":
-                resp = await http_conn.put(url, json=json)
+                resp = await http_conn.put(url, json=json_payload)
             elif request_type == "delete":
                 resp = await http_conn.delete(url)
             elif request_type == "patch":
-                resp = await http_conn.patch(url, json=json)
+                resp = await http_conn.patch(url, json=json_payload)
             else:
                 resp = await http_conn.get(url)
 
