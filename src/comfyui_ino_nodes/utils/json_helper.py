a4506c9cbc6527d12a30bf48d4d872f30599a674
Inoland <contact@nobandegan.com>
2025-10-09 12:31:24 +0400

    Update json_helper.py

--- src/comfyui_ino_nodes/utils/json_helper.py
+++ src/comfyui_ino_nodes/utils/json_helper.py
@@ -1,5 +1,8 @@
 import json
 from copy import deepcopy
+from pathlib import Path
+
+import folder_paths
 
 from inopyutils import InoJsonHelper
 
@@ -7,7 +10,6 @@ from ..node_helper import any_typ
 
 class InoJson:
     """
-
     """
 
     @classmethod
@@ -20,21 +22,56 @@ class InoJson:
             }
         }
 
-    RETURN_TYPES = ("STRING", )
-    RETURN_NAMES = ("Json", )
+    RETURN_TYPES = ("BOOLEAN", "STRING", "STRING", )
+    RETURN_NAMES = ("Success", "MSG", "Json", )
 
     FUNCTION = "function"
     CATEGORY = "InoNodes"
 
     def function(self, base_json, field_name, field_value):
         json_object = InoJsonHelper.string_to_dict(base_json)
+        if not json_object["success"]:
+            return (False, json_object["msg"], "", )
+        json_object = json_object["data"]
+
         json_object[field_name] = field_value
-        json_string = json.dumps(json_object)
-        return (json_string, )
+        json_string = InoJsonHelper.dict_to_string(json_object)
+
+        return (json_string["success"], json_string["msg"], json_string["data"], )
+
+class InoSaveJson:
+    """
+    """
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "json_string": ("STRING", {"default": "{}"}),
+                "local_path": ("STRING", {"default": ""}),
+            }
+        }
+
+    RETURN_TYPES = ("BOOLEAN", "STRING", )
+    RETURN_NAMES = ("Success", "MSG",  )
+
+    FUNCTION = "function"
+    CATEGORY = "InoNodes"
+
+    async def function(self, json_string, local_path):
+        output_path = folder_paths.get_output_directory()
+        save_path :Path = Path(output_path) / Path(local_path)
+        save_json = await InoJsonHelper.save_string_as_json(
+            json_string=json_string,
+            file_path=str(save_path.resolve())
+        )
+        return (save_json["success"], save_json["msg"], )
 
 LOCAL_NODE_CLASS = {
     "InoJson": InoJson,
+    "InoSaveJson": InoSaveJson,
 }
 LOCAL_NODE_NAME = {
     "InoJson": "Ino Json",
+    "InoSaveJson": "Ino Save Json",
 }
