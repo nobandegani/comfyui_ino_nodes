5cd1b5bb3c457c6853cf1dadaa45b733bb733f19
Inoland <contact@nobandegan.com>
2025-10-19 11:29:08 +0400

    add openai helper

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
 """Top-level package for comfyui_ino_nodes."""
-from .src.comfyui_ino_nodes.node_utils import LOCAL_NODE_CLASS as UTILS_CLASS, LOCAL_NODE_NAME as UTILS_NAME
-from .src.comfyui_ino_nodes.node_cloudreve import LOCAL_NODE_CLASS as CLOUDREVE_CLASS, LOCAL_NODE_NAME as CLOUDREVE_NAME
-
 from .src.comfyui_ino_nodes.s3_helper import LOCAL_NODE_CLASS as S3_CLASS, LOCAL_NODE_NAME as S3_NAME
 
 from .src.comfyui_ino_nodes.utils.extra_nodes import LOCAL_NODE_CLASS as EXTRA_CLASS, LOCAL_NODE_NAME as EXTRA_NAME
@@ -11,10 +8,9 @@ from .src.comfyui_ino_nodes.utils.lora_helper import LOCAL_NODE_CLASS as LORA_CL
 from .src.comfyui_ino_nodes.utils.prompt_helper import LOCAL_NODE_CLASS as PROMPT_CLASS, LOCAL_NODE_NAME as PROMPT_NAME
 from .src.comfyui_ino_nodes.utils.sampler_helper import LOCAL_NODE_CLASS as SAMPLER_CLASS, LOCAL_NODE_NAME as SAMPLER_NAME
 from .src.comfyui_ino_nodes.utils.file_helper import LOCAL_NODE_CLASS as FILE_CLASS, LOCAL_NODE_NAME as FILE_NAME
+from .src.comfyui_ino_nodes.utils.openai_helper import LOCAL_NODE_CLASS as OPENAPI_CLASS, LOCAL_NODE_NAME as OPENAPI_NAME
 
 _node_classes ={}
-_node_classes.update(UTILS_CLASS)
-_node_classes.update(CLOUDREVE_CLASS)
 _node_classes.update(S3_CLASS)
 _node_classes.update(EXTRA_CLASS)
 _node_classes.update(JSON_CLASS)
@@ -23,10 +19,9 @@ _node_classes.update(LORA_CLASS)
 _node_classes.update(PROMPT_CLASS)
 _node_classes.update(SAMPLER_CLASS)
 _node_classes.update(FILE_CLASS)
+_node_classes.update(OPENAPI_CLASS)
 
 _node_names = {}
-_node_names.update(UTILS_NAME)
-_node_names.update(CLOUDREVE_NAME)
 _node_names.update(S3_NAME)
 _node_names.update(EXTRA_NAME)
 _node_names.update(JSON_NAME)
@@ -35,6 +30,7 @@ _node_names.update(LORA_NAME)
 _node_names.update(PROMPT_NAME)
 _node_names.update(SAMPLER_NAME)
 _node_names.update(FILE_NAME)
+_node_names.update(OPENAPI_NAME)
 
 NODE_CLASS_MAPPINGS = _node_classes
 NODE_DISPLAY_NAME_MAPPINGS = _node_names
---
+++ src/comfyui_ino_nodes/utils/openai_helper.py
@@ -0,0 +1,101 @@
+import hashlib
+import random
+
+from openai import OpenAI
+
+openai_client = None
+
+def get_openai_client(config):
+    global openai_client
+    if openai_client is None:
+        openai_client = OpenAI(
+            api_key=config["api_key"],
+            timeout=config["timeout"],
+            max_retries=config["max_retries"],
+        )
+    return openai_client
+
+
+class InoOpenaiConfig:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "openai_api_key": ("STRING", {"default": "sk-proj-xxxxxxx"}),
+                "timeout": ("FLOAT", {"default": 300}),
+                "max_retries": ("INT", {"default": 3}),
+            }
+        }
+
+    CATEGORY = "InoOpenaiHelper"
+    RETURN_TYPES = ("BOOLEAN", "STRING",)
+    RETURN_NAMES = ("success", "config",)
+    FUNCTION = "function"
+
+    async def function(self, openai_api_key, timeout, max_retries):
+        config = {
+            "api_key": openai_api_key,
+            "timeout": timeout,
+            "max_retries": max_retries,
+        }
+        return (True, config)
+
+class InoOpenaiTextGeneration:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "enabled": ("BOOLEAN", {"default": True, "label_off": "OFF", "label_on": "ON"}),
+                "seed": ("INT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 0xffffffffffffffff,
+                    "step": 1,
+                    "label": "Seed (0 = random)"
+                }),
+                "config": ("STRING", {"default": ""}),
+                "prompt": ("STRING", {"default": ""}),
+                "model": ("STRING", {"default": "gpt-5"}),
+            }
+        }
+
+    CATEGORY = "InoOpenaiHelper"
+    RETURN_TYPES = ("BOOLEAN", "INT", "STRING", "STRING", "STRING", "STRING",)
+    RETURN_NAMES = ("success", "id", "status", "error", "output_text", "output",)
+    FUNCTION = "function"
+
+    @classmethod
+    def IS_CHANGED(cls, seed, **kwargs):
+        m = hashlib.sha256()
+        m.update(seed)
+        return m.digest().hex()
+
+    async def function(self, enabled, seed, config, prompt, model):
+        if not enabled:
+            return (False, )
+
+        client = get_openai_client(config)
+
+        response = client.responses.create(
+            model=model,
+            input=prompt
+        )
+
+        if response.error:
+            error_message = response.error.message
+        else:
+            error_message = "none"
+
+        #response.output
+
+        return (True, response.id, response.status, error_message, response.output_text, "", )
+
+
+LOCAL_NODE_CLASS = {
+    "InoOpenaiConfig": InoOpenaiConfig,
+    "InoOpenaiTextGeneration": InoOpenaiTextGeneration,
+}
+LOCAL_NODE_NAME = {
+    "InoOpenaiConfig": "Ino Openai Config",
+    "InoOpenaiTextGeneration": "Ino Openai Text Generation",
+}
