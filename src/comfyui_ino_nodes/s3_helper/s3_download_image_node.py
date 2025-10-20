9d5b5485081006a6dc23a7e900711e92a89544ad
Inoland <contact@nobandegan.com>
2025-10-20 18:48:11 +0400

    Update s3_download_image_node.py

--- src/comfyui_ino_nodes/s3_helper/s3_download_image_node.py
+++ src/comfyui_ino_nodes/s3_helper/s3_download_image_node.py
@@ -3,6 +3,12 @@ import torch
 import numpy as np
 from PIL import Image, ImageOps, ImageSequence
 
+from pathlib import Path
+from datetime import datetime
+
+import folder_paths
+import node_helpers
+
 from .s3_helper import S3Helper
 
 class InoS3DownloadImage:
@@ -12,8 +18,9 @@ class InoS3DownloadImage:
             "required": {
                 "s3_config": ("STRING", {"default": ""}),
                 "s3_key": ("STRING", {"default": "input/example.png"}),
-                "save_locally": ("BOOLEAN", {"default": True}),
-                "save_path": ("STRING", {"default": "input/example.png"})
+            },
+            "optional": {
+                "bucket_name": ("STRING", {"default": "default"}),
             }
         }
 
@@ -22,44 +29,68 @@ class InoS3DownloadImage:
     RETURN_NAMES = ("success", "msg", "result", "image", "mask", )
     FUNCTION = "function"
 
-    async def function(self, s3_key, save_locally, save_path, s3_config):
-        rel_path = S3Helper.get_save_path(s3_key, save_path)
-        abs_path = rel_path.resolve()
+    async def function(self, s3_config, s3_key, bucket_name):
+        validate_s3_config = S3Helper.validate_s3_config(s3_config)
+        if not validate_s3_config["success"]:
+            return (False, validate_s3_config["msg"], "", 0, 0, 0, "",)
+
+        validate_s3_key = S3Helper.validate_s3_key(s3_key)
+        if not validate_s3_key["success"]:
+            return (False, validate_s3_key["msg"], "", 0, 0, 0, "",)
+
+        parent_path = folder_paths.get_temp_directory()
+
+        file_name = f'{datetime.now().strftime("%Y%m%d%H%M%S")}{Path(s3_key).suffix}'
+        local_save_path: Path = Path(parent_path) / file_name
 
         s3_instance = S3Helper.get_instance(s3_config)
         downloaded = await s3_instance.download_file(
             s3_key=s3_key,
-            local_file_path=str(rel_path)
+            local_file_path=str(local_save_path.resolve())
         )
         if not downloaded["success"]:
             return (downloaded["success"], downloaded["msg"], downloaded, None, None, )
 
-        img = Image.open(rel_path)
+        img = node_helpers.pillow(Image.open, downloaded["local_file"])
+
         output_images = []
         output_masks = []
+        w, h = None, None
+
+        excluded_formats = ['MPO']
+
         for i in ImageSequence.Iterator(img):
-            i = ImageOps.exif_transpose(i)
+            i = node_helpers.pillow(ImageOps.exif_transpose, i)
+
             if i.mode == 'I':
                 i = i.point(lambda i: i * (1 / 255))
             image = i.convert("RGB")
+
+            if len(output_images) == 0:
+                w = image.size[0]
+                h = image.size[1]
+
+            if image.size[0] != w or image.size[1] != h:
+                continue
+
             image = np.array(image).astype(np.float32) / 255.0
             image = torch.from_numpy(image)[None,]
             if 'A' in i.getbands():
                 mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            elif i.mode == 'P' and 'transparency' in i.info:
+                mask = np.array(i.convert('RGBA').getchannel('A')).astype(np.float32) / 255.0
                 mask = 1. - torch.from_numpy(mask)
             else:
                 mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
             output_images.append(image)
             output_masks.append(mask.unsqueeze(0))
 
-        if len(output_images) > 1:
+        if len(output_images) > 1 and img.format not in excluded_formats:
             output_image = torch.cat(output_images, dim=0)
             output_mask = torch.cat(output_masks, dim=0)
         else:
             output_image = output_images[0]
             output_mask = output_masks[0]
-
-        if not save_locally:
-            os.remove(rel_path)
 
         return (downloaded["success"], downloaded["msg"], downloaded, output_image, output_mask, )
