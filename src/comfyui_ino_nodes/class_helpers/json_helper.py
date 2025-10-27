6e5570fc58cc4e4fb76b90db7255ec3b69ffe046
Inoland <contact@nobandegan.com>
2025-10-27 19:21:31 +0400

    add print log to json helper

--- src/comfyui_ino_nodes/class_helpers/json_helper.py
+++ src/comfyui_ino_nodes/class_helpers/json_helper.py
@@ -6,7 +6,8 @@ import folder_paths
 
 from inopyutils import InoJsonHelper
 
-from ..node_helper import any_type
+from ..node_helper import any_type, ino_print_log
+
 
 class InoJsonSetField:
     """
@@ -29,18 +30,25 @@ class InoJsonSetField:
     CATEGORY = "InoNodes"
 
     def function(self, base_json, field_name, field_value):
-        json_object = InoJsonHelper.string_to_dict(base_json)
-        if not json_object["success"]:
-            return (False, json_object["msg"], "", )
-        json_object = json_object["data"]
+        try:
+            json_object = InoJsonHelper.string_to_dict(base_json)
+            if not json_object["success"]:
+                ino_print_log("InoJsonSetField", json_object["msg"])
+                return (False, json_object["msg"], "", )
+            json_object = json_object["data"]
 
-        json_object[field_name] = field_value
-        json_string = InoJsonHelper.dict_to_string(json_object)
-        if not json_string["success"]:
-            return (False, json_string["msg"], "", )
+            json_object[field_name] = field_value
+            json_string = InoJsonHelper.dict_to_string(json_object)
+            if not json_string["success"]:
+                ino_print_log("InoJsonSetField", json_string["msg"])
+                return (False, json_string["msg"], "", )
 
-        base_json = json_string["data"]
-        return (True, "Success", base_json, )
+            base_json = json_string["data"]
+            ino_print_log("InoJsonSetField", "Success")
+            return (True, "Success", base_json, )
+        except Exception as e:
+            ino_print_log("InoJsonSetField", "",str(e))
+            return (False, f"failed: {e}", "",)
 
 class InoJsonGetField:
     """
@@ -62,12 +70,18 @@ class InoJsonGetField:
     CATEGORY = "InoNodes"
 
     def function(self, base_json, field_name):
-        json_object = InoJsonHelper.string_to_dict(base_json)
-        if not json_object["success"]:
-            return (False, json_object["msg"], "", )
-        json_object = json_object["data"]
+        try:
+            json_object = InoJsonHelper.string_to_dict(base_json)
+            if not json_object["success"]:
+                ino_print_log("InoJsonGetField", json_object["msg"])
+                return (False, json_object["msg"], "", )
+            json_object = json_object["data"]
 
-        return (True, "Success", json_object[field_name], )
+            ino_print_log("InoJsonGetField", "Success")
+            return (True, "Success", json_object[field_name], )
+        except Exception as e:
+            ino_print_log("InoJsonGetField", "",str(e))
+            return (False, f"failed: {e}", "",)
 
 class InoSaveJson:
     """
@@ -89,13 +103,23 @@ class InoSaveJson:
     CATEGORY = "InoNodes"
 
     async def function(self, json_string, local_path):
-        output_path = folder_paths.get_output_directory()
-        save_path :Path = Path(output_path) / Path(local_path)
-        save_json = await InoJsonHelper.save_string_as_json_async(
-            json_string=json_string,
-            file_path=str(save_path.resolve())
-        )
-        return (save_json["success"], save_json["msg"], )
+        try:
+            output_path = folder_paths.get_output_directory()
+            save_path :Path = Path(output_path) / Path(local_path)
+            save_json = await InoJsonHelper.save_string_as_json_async(
+                json_string=json_string,
+                file_path=str(save_path.resolve())
+            )
+
+            if not save_json["success"]:
+                ino_print_log("InoSaveJson", save_json["msg"])
+                return (False, save_json["msg"],)
+
+            ino_print_log("InoSaveJson", "Success")
+            return (True, save_json["msg"], )
+        except Exception as e:
+            ino_print_log("InoSaveJson", "",str(e))
+            return (False, f"failed: {e}", "",)
 
 LOCAL_NODE_CLASS = {
     "InoJsonSetField": InoJsonSetField,
