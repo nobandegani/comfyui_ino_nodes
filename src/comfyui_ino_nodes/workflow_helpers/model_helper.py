bf67b429f954756865f03ae4fa712e05707ef1c7
Inoland <contact@nobandegan.com>
2025-10-27 19:17:35 +0400

    add print log to model helper

--- src/comfyui_ino_nodes/workflow_helpers/model_helper.py
+++ src/comfyui_ino_nodes/workflow_helpers/model_helper.py
@@ -9,6 +9,7 @@ import node_helpers
 
 
 from ..s3_helper.s3_helper import S3Helper
+from ..node_helper import ino_print_log
 
 MODEL_TYPES = (
     "checkpoints", "clip", "clip_vision", "controlnet", "diffusers", "diffusion_models",
@@ -39,7 +40,7 @@ class InoValidateModel:
 
         parent_path = Path(folder_paths.get_input_directory()).parent
         model_path:Path = parent_path / "models" / model_type / model_name
-        print(model_path)
+
         need_download = not model_path.is_file()
         if not need_download:
             return (True, "model validated", )
@@ -91,50 +92,56 @@ class InoHuggingFaceDownloadFile:
     FUNCTION = "function"
 
     async def function(self, enabled, dict_as_input, model_type="", model_subfolder="", repo_id="", filename="", subfolder="", token="", repo_type="", revision=""):
-        if not enabled:
-            return (False, "", "", )
+        try:
+            if not enabled:
+                ino_print_log("InoHuggingFaceDownloadFile", "Attempt to run but disabled")
+                return (False, "", "", )
 
-        if isinstance(dict_as_input, str):
-            input_config = InoJsonHelper.string_to_dict(dict_as_input)
-            if input_config["success"]:
-                dict_as_input = input_config["data"]
+            if isinstance(dict_as_input, str):
+                input_config = InoJsonHelper.string_to_dict(dict_as_input)
+                if input_config["success"]:
+                    dict_as_input = input_config["data"]
 
-        if isinstance(dict_as_input, dict):
-            model_type = dict_as_input.get("model_type", model_type)
-            model_subfolder = dict_as_input.get("model_subfolder", model_subfolder)
-            repo_id = dict_as_input.get("repo_id", repo_id)
-            filename = dict_as_input.get("filename", filename)
-            subfolder = dict_as_input.get("subfolder", subfolder)
-            repo_type = dict_as_input.get("repo_type", repo_type)
-            revision = dict_as_input.get("revision", revision)
+            if isinstance(dict_as_input, dict):
+                model_type = dict_as_input.get("model_type", model_type)
+                model_subfolder = dict_as_input.get("model_subfolder", model_subfolder)
+                repo_id = dict_as_input.get("repo_id", repo_id)
+                filename = dict_as_input.get("filename", filename)
+                subfolder = dict_as_input.get("subfolder", subfolder)
+                repo_type = dict_as_input.get("repo_type", repo_type)
+                revision = dict_as_input.get("revision", revision)
 
 
-        parent_path = Path(folder_paths.get_input_directory()).parent
-        model_path: Path = parent_path / "models" / model_type / model_subfolder
-        if model_path.is_file():
-            model_path = model_path.parent
+            parent_path = Path(folder_paths.get_input_directory()).parent
+            model_path: Path = parent_path / "models" / model_type / model_subfolder
+            if model_path.is_file():
+                model_path = model_path.parent
 
-        args = {}
-        if subfolder:
-            args["subfolder"] = subfolder
-        if token:
-            args["token"] = token
-        if repo_type:
-            args["repo_type"] = repo_type
-        if revision:
-            args["revision"] = revision
+            args = {}
+            if subfolder:
+                args["subfolder"] = subfolder
+            if token:
+                args["token"] = token
+            if repo_type:
+                args["repo_type"] = repo_type
+            if revision:
+                args["revision"] = revision
 
-        args["local_dir"] = model_path
+            args["local_dir"] = model_path
+        except Exception as e:
+            ino_print_log("InoHuggingFaceDownloadFile", "", e)
+            return (False, f"Error: {e}", "",)
 
         try:
             result = hf_hub_download(repo_id, filename, **args)
             rel_path = Path(result).relative_to(model_path.parent)
+            ino_print_log("InoHuggingFaceDownloadFile", "file downloaded successfully", )
             return (True, "Successfull", model_type, result, rel_path)
         except Exception as e:
+            ino_print_log("InoHuggingFaceDownloadFile", "", e)
             return (False, f"Error: {e}", "", )
 
 class InoCivitaiDownloadFile:
-
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -160,98 +167,109 @@ class InoCivitaiDownloadFile:
 
     async def function(self, enabled, dict_as_input, model_type="", model_subfolder="", model_version="", token="", model_id="", file_id=0, chunk_size=8):
         if not enabled:
+            ino_print_log("InoCivitaiDownloadFile", "Attempt to run but disabled")
             return (False, "", "", "", "", )
 
-        if isinstance(dict_as_input, str):
-            input_config = InoJsonHelper.string_to_dict(dict_as_input)
-            if input_config["success"]:
-                dict_as_input = input_config["data"]
-
-        if isinstance(dict_as_input, dict):
-            model_type = dict_as_input.get("model_type", model_type)
-            model_subfolder = dict_as_input.get("model_subfolder", model_subfolder)
-            model_version = dict_as_input.get("revision", model_version)
-            model_id = dict_as_input.get("repo_id", model_id)
-            file_id = dict_as_input.get("filename", file_id)
-
-        parent_path = Path(folder_paths.get_input_directory()).parent
-        model_path: Path = parent_path / "models" / model_type / model_subfolder
-        if model_path.is_file():
-            model_path = model_path.parent
-
-        token = os.getenv("CIVITAI_TOKEN", token)
-
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
-
-        http_client = InoHttpHelper(
-            timeout_total=None,
-            timeout_sock_read=None,
-            timeout_connect=30.0,
-            timeout_sock_connect=30.0,
-            retries=5,
-            backoff_factor=1.0,
-            default_headers=headers
-        )
-
-        if model_version:
-            url = f"https://civitai.com/api/v1/model-versions/{model_version}"
-        else:
-            await http_client.close()
-            return (False, "model_version is required, using model_id not implemented yet", "", "", "",)
-
-        download_url = await http_client.get(
-            url=url
-        )
-        if not download_url["success"]:
-            await http_client.close()
-            return (False, download_url["msg"], "", "", "",)
-
         try:
-            file_name = download_url["data"]["files"][file_id]["name"]
-            file_sha = download_url["data"]["files"][file_id]["hashes"]["SHA256"]
-        except:
-            await http_client.close()
-            return (False, "files is empty", "", "", "",)
+            if isinstance(dict_as_input, str):
+                input_config = InoJsonHelper.string_to_dict(dict_as_input)
+                if input_config["success"]:
+                    dict_as_input = input_config["data"]
 
-        full_model_path = model_path / file_name
+            if isinstance(dict_as_input, dict):
+                model_type = dict_as_input.get("model_type", model_type)
+                model_subfolder = dict_as_input.get("model_subfolder", model_subfolder)
+                model_version = dict_as_input.get("revision", model_version)
+                model_id = dict_as_input.get("repo_id", model_id)
+                file_id = dict_as_input.get("filename", file_id)
 
-        if full_model_path.is_file():
-            sha_res = await InoFileHelper.get_file_hash_sha_256(full_model_path)
-            if sha_res["success"] and sha_res["sha"].lower() == file_sha.lower():
-                rel_path = full_model_path.relative_to(model_path.parent)
-                await http_client.close()
-                return (True, "File is valid", model_type, full_model_path, rel_path, )
+            parent_path = Path(folder_paths.get_input_directory()).parent
+            model_path: Path = parent_path / "models" / model_type / model_subfolder
+            if model_path.is_file():
+                model_path = model_path.parent
+
+            token = os.getenv("CIVITAI_TOKEN", token)
+
+            headers = {
+                "Authorization": f"Bearer {token}"
+            }
+
+            http_client = InoHttpHelper(
+                timeout_total=None,
+                timeout_sock_read=None,
+                timeout_connect=30.0,
+                timeout_sock_connect=30.0,
+                retries=5,
+                backoff_factor=1.0,
+                default_headers=headers
+            )
+
+            if model_version:
+                url = f"https://civitai.com/api/v1/model-versions/{model_version}"
             else:
-                full_model_path.unlink()
+                await http_client.close()
+                ino_print_log("InoCivitaiDownloadFile", "model_version is required, using model_id not implemented yet")
+                return (False, "model_version is required, using model_id not implemented yet", "", "", "",)
 
-        if not download_url["data"]["downloadUrl"]:
+            download_url = await http_client.get(
+                url=url
+            )
+            if not download_url["success"]:
+                await http_client.close()
+                ino_print_log("InoCivitaiDownloadFile", "failed to get download url", download_url["msg"])
+                return (False, download_url["msg"], "", "", "",)
+
+            try:
+                file_name = download_url["data"]["files"][file_id]["name"]
+                file_sha = download_url["data"]["files"][file_id]["hashes"]["SHA256"]
+            except Exception as e:
+                await http_client.close()
+                ino_print_log("InoCivitaiDownloadFile", "failed to get file name or sha", e)
+                return (False, "files is empty", "", "", "",)
+
+            full_model_path = model_path / file_name
+
+            if full_model_path.is_file():
+                sha_res = await InoFileHelper.get_file_hash_sha_256(full_model_path)
+                if sha_res["success"] and sha_res["sha"].lower() == file_sha.lower():
+                    rel_path = full_model_path.relative_to(model_path.parent)
+                    await http_client.close()
+                    ino_print_log("InoCivitaiDownloadFile", "File already downloaded and valid", )
+                    return (True, "File is valid", model_type, full_model_path, rel_path, )
+                else:
+                    full_model_path.unlink()
+
+            if not download_url["data"]["downloadUrl"]:
+                await http_client.close()
+                ino_print_log("InoCivitaiDownloadFile", "downloadUrl is empty", )
+                return (False, "downloadUrl is empty", "", "", "",)
+
+            download_url = download_url["data"]["files"][file_id]["downloadUrl"]
+
+            download_file = await http_client.download(
+                url=download_url,
+                dest_path=model_path,
+                chunk_size=chunk_size * 1024 * 1024,
+                resume=True,
+                overwrite=False,
+                allow_redirects=True,
+                mkdirs=True,
+                verify_size=True,
+            )
+
+            if not download_file["success"] and download_file["success"] != "OK":
+                await http_client.close()
+                ino_print_log("InoCivitaiDownloadFile", "failed to download file", download_file["msg"])
+                return (download_file["success"], download_file["msg"], "", "", "",)
+
+            rel_path = Path(download_file["path"]).relative_to(model_path.parent)
+
             await http_client.close()
-            return (False, "downloadUrl is empty", "", "", "",)
-
-        download_url = download_url["data"]["files"][file_id]["downloadUrl"]
-
-        download_file = await http_client.download(
-            url=download_url,
-            dest_path=model_path,
-            chunk_size=chunk_size * 1024 * 1024,
-            resume=True,
-            overwrite=False,
-            allow_redirects=True,
-            mkdirs=True,
-            verify_size=True,
-        )
-
-        if not download_file["success"] and download_file["success"] != "OK":
-            await http_client.close()
-            return (download_file["success"], download_file["msg"], "", "", "",)
-
-        rel_path = Path(download_file["path"]).relative_to(model_path.parent)
-
-        await http_client.close()
-        return (download_file["success"], "File is downloaded", model_type, download_file["path"], rel_path, )
-
+            ino_print_log("InoCivitaiDownloadFile", "File downloaded successfully", )
+            return (download_file["success"], "File is downloaded", model_type, download_file["path"], rel_path, )
+        except Exception as e:
+            ino_print_log("InoCivitaiDownloadFile", "", e)
+            return (False, e, "", "", "",)
 
 LOCAL_NODE_CLASS = {
     "InoValidateModel": InoValidateModel,
