604828ea50bf6881dfb004b0bc35e3971fd45308
Inoland <contact@nobandegan.com>
2025-11-01 23:03:32 +0400

    Update node_helper.py

--- src/comfyui_ino_nodes/node_helper.py
+++ src/comfyui_ino_nodes/node_helper.py
@@ -24,3 +24,7 @@ MODEL_TYPES = (
     "embeddings", "face_restore", "loras", "sams", "style_models","text_encoders", "transformers",
     "tts", "unet", "upscale_models", "vae", "vibevoice", "wav2vec2"
 )
+
+UNET_WEIGHT_DTYPE=["default", "fp8_e4m3fn", "fp8_e4m3fn_fast", "fp8_e5m2"]
+
+CLIP_TYPE= ["stable_diffusion", "stable_cascade", "sd3", "stable_audio", "mochi", "ltxv", "pixart", "cosmos", "lumina2", "wan", "hidream", "chroma", "ace", "omnigen2", "qwen_image", "hunyuan_image"]
