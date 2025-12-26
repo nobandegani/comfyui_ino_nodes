976f45f64cfb1e344c198cd872f806bb67bff01b
Inoland <contact@nobandegan.com>
2025-12-26 19:55:15 +0400

    update

---
+++ src/comfyui_ino_nodes/node_helpers/util_helper.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+from inopyutils import InoJsonHelper, ino_is_err
+
+import folder_paths
+
+from comfy_api.latest import ComfyExtension, io, ui
+from custom_nodes.comfyui_ino_nodes.src.comfyui_ino_nodes.class_helpers.json_helper import InoJsonSetField
+
+
+
+
+LOCAL_NODE_CLASS = {
+
+}
+LOCAL_NODE_NAME = {
+}
