fa784cb809a2409bfa6ea35204ed35efbe84855b
Inoland <contact@nobandegan.com>
2025-12-27 17:54:34 +0400

    Update init_helper.py

--- src/comfyui_ino_nodes/init_helper.py
+++ src/comfyui_ino_nodes/init_helper.py
@@ -2,30 +2,30 @@ import asyncio
 
 from .workflow_helpers.download_model_helper import InoHuggingFaceDownloadModel
 
-def init_models():
+async def init_models():
     huggingface_downloader = InoHuggingFaceDownloadModel()
 
-    wan_animate_yolo = asyncio.run(huggingface_downloader.function(
+    wan_animate_yolo = await huggingface_downloader.function(
         enabled=True,
         model_config="{}",
         model_type="detection",
         model_subfolder="",
         repo_id="Wan-AI/Wan2.2-Animate-14B",
         filename="process_checkpoint/det/yolov10m.onnx",
-    ))
-    wan_animate_vitpose_bin = asyncio.run(huggingface_downloader.function(
+    )
+    wan_animate_vitpose_bin = await huggingface_downloader.function(
         enabled=True,
         model_config="{}",
         model_type="detection",
         model_subfolder="",
         repo_id="Kijai/vitpose_comfy",
         filename="onnx/vitpose_h_wholebody_data.bin",
-    ))
-    wan_animate_vitpose_onnx = asyncio.run(huggingface_downloader.function(
+    )
+    wan_animate_vitpose_onnx = await huggingface_downloader.function(
         enabled=True,
         model_config="{}",
         model_type="detection",
         model_subfolder="",
         repo_id="Kijai/vitpose_comfy",
         filename="onnx/vitpose_h_wholebody_model.onnx",
-    ))
+    )
