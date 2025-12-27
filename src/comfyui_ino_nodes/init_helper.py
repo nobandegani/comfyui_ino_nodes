3f57470da42b2e00a48cfc4351e98cd2f5abe057
Inoland <contact@nobandegan.com>
2025-12-27 18:16:17 +0400

    Update init_helper.py

--- src/comfyui_ino_nodes/init_helper.py
+++ src/comfyui_ino_nodes/init_helper.py
@@ -1,11 +1,32 @@
 import asyncio
 
-from .workflow_helpers.download_model_helper import InoHuggingFaceDownloadModel
+from .workflow_helpers.download_model_helper import InoHuggingFaceDownloadModel, InoGetVideoModelDownloadConfig, InoGetClipDownloadConfig, InoGetVaeDownloadConfig, InoHandleDownloadModel
 
 async def init_models():
     print("Init models started.")
 
+    video_model_config_loader = InoGetVideoModelDownloadConfig()
+    clip_model_config_loader = InoGetClipDownloadConfig()
+    vae_model_config_loader = InoGetVaeDownloadConfig()
+
+    handle_download_model = InoHandleDownloadModel()
+
     huggingface_downloader = InoHuggingFaceDownloadModel()
+
+    wan_animate_model_bf16 = handle_download_model.function(
+        enabled=True,
+        config=video_model_config_loader.function(True, "comfy-Wan-22-Animate-14B-bf16"),
+    )
+
+    wan_22_clip_bf16 = handle_download_model.function(
+        enabled=True,
+        config=clip_model_config_loader.function(True, "kj-umt5-bf16"),
+    )
+
+    wan_21_vae_bf16 = handle_download_model.function(
+        enabled=True,
+        config=vae_model_config_loader.function(True, "kj-wan-21-bf16"),
+    )
 
     wan_animate_yolo = await huggingface_downloader.function(
         enabled=True,
